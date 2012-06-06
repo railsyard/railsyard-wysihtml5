60c7bd3bd0470e084a76451ad686c5dbd001a553
Stefano Verna <stefano.verna@welaika.com>
2012-06-06 17:54:31 +0200

    Added precompilation asset

--- lib/railsyard/wysihtml5/engine.rb
+++ lib/railsyard/wysihtml5/engine.rb
@@ -1,6 +1,14 @@
 module Railsyard
   module Wysihtml5
     class Engine < ::Rails::Engine
+
+      initializer "Railsyard precompile hook", :group => :assets do |app|
+        app.config.assets.precompile += [
+          "railsyard/admin/railsyard_wysihtml5.js",
+          "railsyard/admin/railsyard_wysihtml5.css"
+        ]
+      end
+
       config.to_prepare do
         Railsyard::Backend.plugin_manager.add_plugin(:wysihtml5) do
           name "Wysihtml5"
@@ -8,6 +16,7 @@ module Railsyard
           backend_stylesheet_dependency "railsyard/admin/railsyard_wysihtml5"
         end
       end
+
     end
   end
 end
