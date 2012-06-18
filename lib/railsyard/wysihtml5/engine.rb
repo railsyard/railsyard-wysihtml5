4625b768763a9438ea65fd47c66d17f6468902b9
nebirhos <nebirhos@aol.com>
2012-06-18 18:04:00 +0200

    Changed custom generator syntax

--- lib/railsyard/wysihtml5/engine.rb
+++ lib/railsyard/wysihtml5/engine.rb
@@ -10,17 +10,19 @@ module Railsyard
       end
 
       config.to_prepare do
-        Railsyard::Backend.plugin_manager.add_plugin(:wysihtml5) do
-          name "Wysihtml5"
-          backend_javascript_dependency "railsyard/admin/railsyard_wysihtml5"
-          backend_stylesheet_dependency "railsyard/admin/railsyard_wysihtml5"
-
-          generator_editor_type :wysihtml5, :text, <<DSL
-as: :wysihtml5 do
+        editor_config = <<DSL
+field :%{name}, as: :wysihtml5 do
       input_options toolbar:
         {commands: [:bold, :italic, :link, :image, :unordered_list, :ordered_list, :source]} # any of these
     end
 DSL
+        Railsyard::Backend.plugin_manager.add_plugin(:wysihtml5) do
+          name "Wysihtml5"
+          backend_javascript_dependency "railsyard/admin/railsyard_wysihtml5"
+          backend_stylesheet_dependency "railsyard/admin/railsyard_wysihtml5"
+          generator_editor_type(:wysihtml5,
+                                {"%{name}" => :text},
+                                editor_config)
         end
       end
 
